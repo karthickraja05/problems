eff61587b5a220fbd9e94332b52347396f5d602d
karthickraja05 <karthickraja.gm@gmail.com>
2021-12-16 08:59:40 +0530

    Problem Solved Simple - Karthickraja

--- twoNums.js
+++ twoNums.js
@@ -20,3 +20,14 @@ var twoSum = function(nums, target) {
 
 // sample i/o = [2,7,11,15],9
 // Expected o/p = [0,1]
+
+// Optimized Solution Not Coded By me
+// var twoSum = function(nums, target) {
+//   var map = new Map();
+
+//   for(var i = 0; i<nums.length; i++) {
+//       var num = nums[i];
+//       if(map.get(num) == undefined) map.set(target-num, i);
+//       else return [map.get(num), i];
+//   }
+// };
