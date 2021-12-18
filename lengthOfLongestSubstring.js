6b4ae574bbf129be697e862ef6aed82f7d75b88f
karthickraja05 <karthickraja.gm@gmail.com>
2021-12-18 09:42:19 +0530

    Leet Code Problem Solved with JS

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -7,5 +7,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+    if(s == '') return 0;
+    var n = s.length;
+    var result = 1;
+    var nonRepeatStr = '';
+    for(i = 0; i < n; i++){
+        let index = nonRepeatStr.indexOf(s[i]);
+        if(index !== -1){
+            let len = nonRepeatStr.length;
+            result = result < len ? len : result;
+            nonRepeatStr = nonRepeatStr.slice(index+1);
+        }
+        nonRepeatStr += s[i];
+    }
+    let len = nonRepeatStr.length;
+    return result < len ? len : result;
 };
